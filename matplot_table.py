6bbf63fb055ddd5f95865cc3f40755ceaf51cbc4
vlad <v.cherkashin22@gmail.com>
2022-08-24 15:47:15 +0800

    fix matplot table create

--- matplot_table.py
+++ matplot_table.py
@@ -1,15 +1,16 @@
 from datetime import datetime
 import matplotlib.pyplot as plt
-import pytz
-
+from source_data import tz_irk
 
 def create_table(last_4_db):
     rows = []
     data_table = []
     total_row = [0, 0, 0, 0]
+    time_min = [':15', ':30', ':45', ':00']
 
     for row in last_4_db:
-        rows.append(row[5])
+        time_hours = row[5][0:2]
+        rows.append(time_hours)
         row = list(row)
         row.pop(0)
         row.pop(3)
@@ -19,12 +20,14 @@ def create_table(last_4_db):
         total_row[2] += row[2]/4
         total_row[3] = ''
         data_table.append(row)
-        # print(data_table)
 
     total_row[0] = ('{:.2f}'.format(total_row[0]))
     data_table.append(total_row)
     data = data_table
     val1 = ['Average HS', 'Online', 'Difference', 'Notes', '']
+
+    for i in range(len(rows)):
+        rows[i] = rows[i]+time_min[i]
     rows.append('')
 
     fig, ax = plt.subplots()
@@ -39,7 +42,6 @@ def create_table(last_4_db):
         loc='center left',
         colWidths=[0.3, 0.2, 0.3, 0.2])
 
-    tz_irk = pytz.timezone('Asia/Irkutsk')
     date_ = datetime.now(tz_irk).strftime("%d.%m.%Y")
     time_second = datetime.now(tz_irk).strftime("%H")
     time_first = int(datetime.now(tz_irk).strftime("%H")) - 1
